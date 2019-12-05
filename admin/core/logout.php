80d551515b932be4c331fbe66b18fbd7acdf6225
Nazareno Morillo <nazamorillo1@gmail.com>
2019-12-05 14:17:45 -0300

    gif carga logout arreglado

--- admin/core/logout.php
+++ admin/core/logout.php
@@ -3,7 +3,7 @@ $_SESSION['UserID']=null;
 session_destroy();
 ?>
 <section style="background-color: #fafcf9; height: 100vh;">
-<!-- <div align="center">
-    <img src="../../pics/carga.gif" alt="loading">
-</div> -->
+<div align="center">
+    <img src="pics/carga.gif" alt="loading">
+</div>
 </section>
