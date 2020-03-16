e416a8cfc8e6ef8b132a716ee13e4348cbe7b845
Nazareno Morillo <nazamorillo1@gmail.com>
2020-03-16 02:08:31 -0300

    hecho saludo de usuario logueado

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,30 +1,23 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="#">A-M</a>
     <form class="form-inline mt-2 mt-md-0" style="width: 64%;">
-            <input id="search" class="form-control mr-sm-2" type="text" placeholder="Estoy buscando..." aria-label="Search">
-            <a id="carrito" class="btn btn-outline-success my-2 my-sm-0" href="#"><img width="30px" src="/storage/pics/carrito.png" alt="search" title="Carrito"></a>
-        </form>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <input id="search" class="form-control mr-sm-2" type="text" placeholder="Estoy buscando..." aria-label="Search">
+        <a id="carrito" class="btn btn-outline-success my-2 my-sm-0" href="#"><img width="30px" src="/storage/pics/carrito.png" alt="search" title="Carrito"></a>
+    </form>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item ">
-                <a class="nav-link" href="/">Inicio</a><span class="sr-only">(current)</span>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="/listado">Catalogo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/ayuda">Ayuda</a>
-            </li>
+
 
             @if (Route::has('login'))
             <!-- <div class="top-right links"> -->
             @auth
+            <li class="nav-item">
+                <a href="*" class="nav-link">Bienvenido {{Auth::user()->name}}</a>
+            </li>
             <!-- solo aparece si el usuario está logeado -->
-            <!-- <a href="{{ url('/home') }}">Home</a> -->
-            <!-- <a class="nav-link" href="{{ route('logout') }}">Salir</a> -->
             <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">Salir</a>
             <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
@@ -42,11 +35,26 @@
             </li>
             @endif
             @endauth
-            <!-- </div> -->
+            @guest
+            <li class="nav-item">
+                <a href="*" class="nav-link">No estás logueado</a>
+            </li>
+            
+            @endguest
+
             @endif
 
             <li class="nav-item">
-                <a class="nav-link" href="/contacto">Contacto</a>
+            <li class="nav-item ">
+                <a class="nav-link" href="/">Inicio</a><span class="sr-only">(current)</span>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="/listado">Catalogo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/ayuda">Ayuda</a>
+            </li>
+            <a class="nav-link" href="/contacto">Contacto</a>
             </li>
         </ul>
     </div>
