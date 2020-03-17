4b3056387093cad359cc07c070a41c3403daea1d
Lucio Martínez <luchoweb15@gmail.com>
2020-03-17 10:56:06 -0300

    Roles de Usuarios

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'admin';
+        $role->description = 'Administrador';
+        $role->save();
+
+        $role = new Role();
+        $role->name='user';
+        $role->description = 'usuario normal';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'invited';
+        $role->description = 'usuario invitado';
+        $role->save();
+    }
+}
