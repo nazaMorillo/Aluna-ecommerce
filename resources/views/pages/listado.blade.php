1c7b340dce0dc0e83b6e4bbb9271a9b5cc5e7024
alexiscattaneo <alexiscattaneoimp@gmail.com>
2020-05-08 16:09:27 -0300

    corregida la vista desde guest validando stock

--- resources/views/pages/listado.blade.php
+++ resources/views/pages/listado.blade.php
@@ -164,6 +164,54 @@
                     </div>
                 
             </div>
+                    @elseif($producto->stock == 0)
+                    <div class="card producto col-md-3" id="Producto{{$producto->id}}">
+                <a href="/productos/{{$producto->id}}" style="text-decoration: none; color: rgb(46, 46, 46);">
+                    <img src="/storage/product/{{$producto->image}}" class="card-img-top" alt="{{$producto->id}}" class="card-img-top img-thumbnail" alt="Producto1" style="height: 200px; object-fit: cover;"></a>
+
+                    <div class="card-body">
+                        <h3>$ {{$producto->price}}</h3>
+                        <p class="card-text"> {{$producto->name}}</p>
+                        <!--Función Comprar-->                       
+
+                        <!--<a class='btn btn-primary col-12 col-md-12 text-white mt-2 comprar' role="button" onclick="comprar({{$producto->id}})">Comprar</a>-->
+
+                        <a class='btn bg-black col-12 col-md-12 text-white mt-2 carrito disabled' role="button" href="/login">Sin unidades</a>
+
+                        <!--Fin Función Comprar-->
+                        <!--Agregar al Carrito-->
+
+                        <!--<a id="{{$producto->id}}" class='btn btn-success col-12 col-md-12 text-white mt-2 carrito' role="button" onclick="agregarCarrito({{$producto->id}})">Agregar al carrito</a>-->
+
+                        <!--Fin Agregar al Carrito-->
+                    </div>
+                
+            </div>  
+                    @elseif($producto->stock == 1)
+                    <div class="card producto col-md-3" id="Producto{{$producto->id}}">
+                <a href="/productos/{{$producto->id}}" style="text-decoration: none; color: rgb(46, 46, 46);">
+                    <img src="/storage/product/{{$producto->image}}" class="card-img-top" alt="{{$producto->id}}" class="card-img-top img-thumbnail" alt="Producto1" style="height: 200px; object-fit: cover;"></a>
+
+                    <div class="card-body">
+                        <h3>$ {{$producto->price}}</h3>
+                        <p class="card-text"> {{$producto->name}} (Último disponible!)</p>
+                        <!--Función Comprar-->                       
+
+                        <!--<a class='btn btn-primary col-12 col-md-12 text-white mt-2 comprar' role="button" onclick="comprar({{$producto->id}})">Comprar</a>-->
+
+                        <a class='btn btn-primary col-12 col-md-12 text-white mt-2 comprar' role="button" href="/login">Comprar</a>
+
+                        <!--Fin Función Comprar-->
+                        <!--Agregar al Carrito-->
+
+                        <!--<a id="{{$producto->id}}" class='btn btn-success col-12 col-md-12 text-white mt-2 carrito' role="button" onclick="agregarCarrito({{$producto->id}})">Agregar al carrito</a>-->
+
+                        <a id="{{$producto->id}}" class='btn btn-success col-12 col-md-12 text-white mt-2 carrito' role="button" href="/login">Agregar al carrito</a>
+
+                        <!--Fin Agregar al Carrito-->
+                    </div>
+                
+            </div>                                      
                     @else
                     <div class="card producto col-md-3" id="Producto{{$producto->id}}">
                 <a href="/productos/{{$producto->id}}" style="text-decoration: none; color: rgb(46, 46, 46);">
