4b3d72b68f4904c411c3f934193c8666c7240af2
alexiscattaneo <alexiscattaneoimp@gmail.com>
2020-05-15 19:56:08 -0300

    error merge solucionado

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Allmarket</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/app.css">
